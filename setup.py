778d629d09d188d6ed3fea89b903c79b535f6c48
Arda Arslan <arda@Ardas-MacBook-Air-2.local>
2024-11-13 00:03:52 +0300

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Arda Arslan',
+    author_email= 'arslanhuseyinarda@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
